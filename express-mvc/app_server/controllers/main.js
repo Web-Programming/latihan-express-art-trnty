450e301ef3a75b327ac1e2dd7d4301be87b42772
art-trnty <raraanantabunga_2226240069@mhs.mdp.ac.id>
2024-10-12 08:54:04 +0700

    P10
    controllers

--- express-mvc/app_server/controllers/main.js
+++
@@ -1,9 +0,0 @@
-const index = (req, res) => {
- 	res.render('index', { title: 'Express' }); 
-}; 
-
-//buat controller ke halaman kontak
-const kontak = (req, res) =>  {
-    res.render('/kontak', { title : 'Kontak' });
-}
-module.exports = { index, kontak }; 
